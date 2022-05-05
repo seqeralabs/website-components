de6e481a45704050369be974afdbe6c190fc89df
Oleh Pratsko <olehpratsko@gmail.com>
2022-05-05 17:13:22 +0300

    fix: fix build issues

--- index.jsx
+++ index.jsx
@@ -1,17 +1,9 @@
 import BulletIcon from './components/icons/BulletIcon';
 import CheckIcon from './components/icons/CheckIcon';
-import Link from './components/Link';
-import List from './components/List';
-import Reveal from './components/Reveal';
-import RevealOnScroll from './components/RevealOnScroll';
 
 const Components = {
   BulletIcon,
   CheckIcon,
-  Link,
-  List,
-  Reveal,
-  RevealOnScroll,
 };
 
 export default Components;
