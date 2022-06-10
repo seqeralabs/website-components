d1313f33b2837e6a7db5524ea4bc4a869097fe89
Alvaro Martinez <alvaro.mart.mart+dev@gmail.com>
2022-06-10 12:20:55 +0200

    tweak: add more sample stories

---
+++ components/Button/button-summary.stories.jsx
@@ -0,0 +1,55 @@
+import { Button } from '../..';
+import React from 'react';
+
+ 
+export default {
+  title: 'Components/Button',
+  args: {
+    
+  },
+  argTypes: {
+    
+  },
+};
+
+const Template = (args) => (
+    <div className='flex flex-col gap-3'>
+      <div className="flex gap-3">
+        <Button variant="primary" theme="default" size="lg">Button</Button>
+        <Button variant="primary" theme="default" size="lg" arrow="true" id="primary-hovered">Button</Button>
+        <Button variant="primary" theme="alternative" size="lg">Button</Button>
+        <Button variant="primary" theme="alternative" size="lg" arrow="true" id="alt-hovered">Button</Button>
+      </div>
+      <div className="flex gap-3">
+        <Button variant="secondary" theme="default" size="lg">Button</Button>
+        <Button variant="secondary" theme="default" size="lg" id="secondary-hovered">Button</Button>
+        <Button variant="secondary" theme="alternative" size="lg">Button</Button>
+        <Button variant="secondary" theme="alternative" size="lg" id="alt-hovered">Button</Button>
+      </div>
+      <div className="flex gap-3">
+        <Button variant="primary" theme="default" size="md">Button</Button>
+        <Button variant="primary" theme="default" size="md" arrow="true" id="primary-hovered">Button</Button>
+        <Button variant="primary" theme="alternative" size="md">Button</Button>
+        <Button variant="primary" theme="alternative" size="md" arrow="true" id="alt-hovered">Button</Button>
+      </div>
+      <div className="flex gap-3">
+        <Button variant="secondary" theme="default" size="md">Button</Button>
+        <Button variant="secondary" theme="default" size="md" id="secondary-hovered">Button</Button>
+        <Button variant="secondary" theme="alternative" size="md">Button</Button>
+        <Button variant="secondary" theme="alternative" size="md" id="alt-hovered">Button</Button>
+      </div>
+      <div className="flex gap-3">
+        <Button variant="primary" theme="default" size="sm">Button</Button>
+        <Button variant="primary" theme="default" size="sm" arrow="true" id="primary-hovered">Button</Button>
+        <Button variant="primary" theme="alternative" size="sm">Button</Button>
+        <Button variant="primary" theme="alternative" size="sm" arrow="true" id="alt-hovered">Button</Button>
+      </div>
+      <div className="flex gap-3">
+        <Button variant="secondary" theme="default" size="sm">Button</Button>
+        <Button variant="secondary" theme="default" size="sm" id="secondary-hovered">Button</Button>
+        <Button variant="secondary" theme="alternative" size="sm">Button</Button>
+        <Button variant="secondary" theme="alternative" size="sm" id="alt-hovered">Button</Button>
+      </div>
+    </div>
+  );
+  export const Summary = Template.bind({});
--- components/Button/button.stories.jsx
+++ components/Button/button.stories.jsx
@@ -1,15 +1,18 @@
 import { Button } from '../..';
 import React from 'react';
+
  
 export default {
   title: 'Components/Button',
   component: Button,
   args: {
+    children: 'Button',
     noShadow: false,
     submit: false,
     arrow: false,
   },
   argTypes: {
+    children: { control: 'text'},
     to: { control: 'text' },
     theme:  { options: ['default', 'alternative'] },
     variant: { options: ['reset', 'primary', 'secondary', 'accent'] },
@@ -23,11 +26,31 @@ export default {
 
 const Template = (args) => <Button {...args} />;
 
-export const Submit = Template.bind({});
-Submit.args = {
-  children: `Submit`,
+export const DefaultNoArrow = Template.bind({});
+DefaultNoArrow.storyName = 'Default, primary, without arrow'
+DefaultNoArrow.args = {
+  children: `Button`,
   variant: 'primary',
   theme: 'default',
   size: 'sm',
-  submit: true
+};
+
+export const DefaultWithArrow = Template.bind({});
+DefaultWithArrow.storyName = 'Default, primary, with arrow'
+DefaultWithArrow.args = {
+  children: `Button`,
+  variant: 'primary',
+  theme: 'default',
+  size: 'sm',
+  arrow: true
+};
+
+export const GreenNoArrow = Template.bind({});
+GreenNoArrow.storyName = 'Default, Nextflow, with arrow'
+GreenNoArrow.args = {
+  children: `Button`,
+  variant: 'primary',
+  theme: 'alternative',
+  size: 'sm',
+  arrow: true
 };
