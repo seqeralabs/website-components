07eb65dbf12253a980fc5d54fc6d1f8d1cda6e55
Oleh Pratsko <olehpratsko@gmail.com>
2022-07-04 15:15:03 +0300

    feat: add className prop to Marquee

--- components/Marquee/index.jsx
+++ components/Marquee/index.jsx
@@ -1,8 +1,9 @@
+import classnames from 'classnames';
 import React from 'react';
 
-const Marquee = ({ children }) => {
+const Marquee = ({ children, className }) => {
   return (
-    <div className="bg-black text-white py-2">
+    <div className={classnames('bg-black text-white py-2', className)}>
       <div className="overflow-x-hidden whitespace-nowrap">
         <div className="inline-block relative animate-scroll mr-8">
           {children}
@@ -37,6 +38,16 @@ const Marquee = ({ children }) => {
       </div>
     </div>
   );
+};
+
+Marquee.propTypes = {
+  children: PropTypes.node,
+  className: PropTypes.string,
+};
+
+Marquee.defaultProps = {
+  children: null,
+  className: '',
 };
 
 export default Marquee;
