c995673843c968fd0f03b9dc4b8d33229faba231
Oleh Pratsko <olehpratsko@gmail.com>
2022-08-12 10:16:56 +0300

    fix: fix StarIcon size

--- components/icons/StarIcon.jsx
+++ components/icons/StarIcon.jsx
@@ -13,7 +13,7 @@ const StarIcon = ({ className }) => (
     <svg
         className={className}
         xmlns="http://www.w3.org/2000/svg"
-        viewBox="0 0 24 24"
+        viewBox="0 0 16 16"
     >
         <path
             fillRule="evenodd"
